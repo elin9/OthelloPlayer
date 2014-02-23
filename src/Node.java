dbdc3fb3da3f8ae62fcf4ac514265df606942370
Binley Yang <binleyyang@gmail.com>
2014-02-23 14:31:04 -0500

    next

--- src/Node.java
+++ src/Node.java
@@ -8,7 +8,18 @@ public class Node {
 	int pos;
 	int mobility;
 	Node parent;
-	ArrayList<Node> children= new ArrayList<Node>();
+	ArrayList<Node> children = new ArrayList<Node>();
 	
+	public Node(Move prev, State state, int turn) {
+		this.prev = prev;
+		this.state = state;
+		this.turn = turn;
+	}
 	
+	public void addChild(Node child) {
+		if (children == null)
+			System.out.println("");
+		else
+			children.add(child);
+	}
 }
